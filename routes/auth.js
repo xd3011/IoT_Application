7d24e2ad9c14379e87a8e7591ab5869517a1c3e1
xd3011 <xuandat3011@gmail.com>
2023-12-12 23:16:30 +0700

    Fix API logout

--- routes/auth.js
+++ routes/auth.js
@@ -8,9 +8,7 @@ router.post("/login", authController.login);
 router.post("/refresh", function (req, res) {
     return res.status(200).json("Test");
 });
-router.post("/logout", function (req, res) {
-    return res.status(200).json("Test");
-});
+router.post("/logout", authController.logout);
 router.put("/editPassword/:uid", authController.editPassword);
 
 // router.get("/", authController.get);
